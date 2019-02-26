48b55f86e31ee914f4fb3329e324d8a38554bb61
rubtel97 <46767346+rubtel97@users.noreply.github.com>
2019-02-25 19:06:01 -0500

    Actualizacion de hrefs
    Se actualizaron los hrefs de features.php y usuarios.php para llamar correctamente a las paginas correspondientes.

--- features.php
+++ features.php
@@ -29,15 +29,15 @@
         <div class="sidebar-sticky">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link active" href="#">
+              <a class="nav-link active" href="usuarios.php">
                 <span data-feather="home"></span>
-                Dashboard <span class="sr-only">(current)</span>
+                Usuarios <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="features.php">
                 <span data-feather="file"></span>
-                Orders
+                Features
               </a>
             </li>
             <li class="nav-item">
